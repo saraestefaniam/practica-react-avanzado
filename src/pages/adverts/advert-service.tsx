6314ea870ec58b9a0a366954632fde3a2deba517
Sara <saramf240614@gmail.com>
2025-05-29 16:43:40 -0400

    Creado el service de advert con las funciones para traer la data del backend

---
+++ src/pages/adverts/advert-service.tsx
@@ -0,0 +1,27 @@
+import { client } from "../../api/client";
+
+export const getAdverts = (filters = {}) => {
+  return client.get("/api/v1/adverts", {
+    params: filters,
+  });
+};
+
+export const getAdvertById = (id: string) => {
+  return client.get(`/api/v1/adverts/${id}`);
+};
+
+export const createAdvert = (formData: FormData) => {
+  return client.post("/api/v1/adverts", formData, {
+    headers: {
+      "Content-Type": "multipar/form-data",
+    },
+  });
+};
+
+export const deleteAdvert = (id: string) => {
+  return client.delete(`/api/v1/adverts/${id}`);
+};
+
+export const getAdvertsTags = () => {
+  return client.get("/api/v1/adverts/tags");
+};
