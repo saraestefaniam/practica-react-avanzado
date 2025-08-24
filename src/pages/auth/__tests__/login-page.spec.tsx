9f90dec24bf2567f7097c2a18d6ae7576f215233
Sara <saramf240614@gmail.com>
2025-08-24 11:52:01 -0400

    Creamos test mockeando función de authLogin, además incorporamos la librería de userEvent

--- src/pages/auth/__tests__/login-page.spec.tsx
+++ src/pages/auth/__tests__/login-page.spec.tsx
@@ -1,6 +1,10 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import LoginPage from "../login-page";
 import { Provider } from "react-redux";
+import { authLogin } from "../../../store/actions";
+
+vi.mock("../../../store/actions");
 
 describe("LoginPage", () => {
   const state = {
@@ -37,5 +41,29 @@ describe("LoginPage", () => {
   test("should render", () => {
     const { container } = renderComponent();
     expect(container).toMatchSnapshot();
+  });
+
+  test("should dispatch login", async () => {
+    renderComponent();
+
+    const emailInput = screen.getByLabelText("email");
+    const passwordInput = screen.getByLabelText("password");
+    const button = screen.getByRole("button");
+
+    expect(button).toHaveTextContent("Login");
+    expect(button).toBeDisabled();
+
+    await userEvent.type(emailInput, "sara@example.com");
+    await userEvent.type(passwordInput, "1234");
+    // fireEvent.change(emailInput, { target: { value: "Sara@example.com" } });
+    // fireEvent.change(passwordInput, { target: { value: "1234" } });
+    expect(button).toBeEnabled();
+
+    await userEvent.click(button);
+    //fireEvent.click(button)
+    expect(authLogin).toHaveBeenCalledWith({
+      email: "sara@example.com",
+      password: "1234",
+    });
   });
 });
